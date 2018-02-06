32d522fdacd9f858e5a6ab9b6ea2a959189e8664
Kevin O'Neill <koneill1994@gmail.com>
2018-02-06 14:29:40 -0500

    Final check with Ion, a few minor changes, should be ready to run now

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ Your app description
 class Constants(BaseConstants):
     name_in_url = 'minimum_effort_game'
     players_per_group = 4
-    num_rounds = 10
+    num_rounds = 20
     min_choice = 1
     max_choice = 7
     base_payment = 1
