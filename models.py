975100c4822b8135df95688b23ca14862ec7da50
koneill1994 <koneill1994@gmail.com>
2019-12-17 16:21:05 -0500

    final thing with the condition thing

--- models.py
+++ models.py
@@ -52,10 +52,11 @@ class Group(BaseGroup):
     # 1: upward: +1 and +2 for min and own_choice
     # 2: downward: -1 and -2 for min and own_choice
     # 3: bidirectional: -1 and +1 for min and own_choice
-    condition=random.randrange(0,4) # this chooses randomly
-    # to do: move to group
-    
-    # condition=1 # this will choose a specific condition
+    condition=models.IntegerField()
+    def set_condition(self):
+        c=random.randrange(0,4) # this chooses randomly
+        # c=1 # this will choose a specific condition
+        self.condition = c
 
 
     total_contribution = models.IntegerField()
